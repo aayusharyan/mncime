0aa6a4469ca7ea859fdfeb4a9cfcbdeda330c3ee
Sudha <suda_amir@hotmail.com>
2016-12-05 11:03:05 +0530

    Organising Committee

--- organising_committee.php
+++ organising_committee.php
@@ -96,175 +96,88 @@
 				<div class="heading-underline h3-line">
 						<h3 class="mt-0">Patron</h3>
 					</div>
-            
-              <div class="member col-md-3 col-sm-3 wow fadeInUp">
-                <div class="member-image">
-                  <img src="images/team/team-6.jpg" alt="img">
+				<div class="row mb-40">
+                <div class="col-md-7" style="text-align: justify">
+                  <blockquote class="testimonial-2">
+                    <p style="font-size:17px; line-height: 26px;">I am delighted to announce that our Department of Mechanical Engineering has organized its 5th  National Conference on “Innovations in Mechanical Engineering” during 17th – 18th  March, 2017. This department is always proactively working on activities like project exhibitions, business club, workshops, seminars, expert talks by eminent personalities in the field.<br/>&emsp;&emsp;&emsp; 
+						I am sure that the discussion, deliberations and brain storming sessions conducted in this conference will provide exposure to researchers, academicians and persons from industries. This conference will produce an outcome which may be beneficial for the society in future. <br/>&emsp;&emsp;&emsp;
+						I wish best of luck and grand success to this conference.</p>
+                  </blockquote>
                 </div>
-                <h3>EMMA GRIFFITH</h3>
-                <span>VP ENGINEERING</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="200ms">
-                <div class="member-image">
-                  <img src="images/team/team-8.jpg" alt="img">
+                
+                <div class="col-md-4 col-md-offset-1">
+                  <div class="ts-author-cont">
+                    <div class="ts-author-info">
+                      <div class="ts-name">
+                        <span class="bold">Dr. M. S. Gaikwad</span>
+                      </div>
+						<div class="ts-type">Director STES Campus, Lonavala</div>
+                      <div class="ts-type">Principal, SIT, Lonavala</div>
+                    </div>
+                    <div class="ts-author-img">
+                      <img src="assets/ncime_logo.png" alt="photo">
+                    </div>
+                  </div>
                 </div>
-                <h3>MARY BLACK</h3>
-                <span>HR MANAGER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="400ms">
-                <div class="member-image">
-                  <img src="images/team/team-7.jpg" alt="img">
-                </div>
-                <h3>BRIAN GREEN</h3>
-                <span>ANDROID DEVELOPER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="400ms">
-                <div class="member-image">
-                  <img src="images/team/team-10.jpg" alt="img">
-                </div>
-                <h3>PAUL BISHOP</h3>
-                <span>iOS DEVELOPER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
+          </div>
               
             </div>
 			<div class="row">
 				<div class="heading-underline h3-line">
 						<h3 class="mt-0">Organising Secretary</h3>
 					</div>
-            
-              <div class="member col-md-3 col-sm-3 wow fadeInUp">
-                <div class="member-image">
-                  <img src="images/team/team-6.jpg" alt="img">
+            <div class="row mb-40">
+                <div class="col-md-7" style="text-align: justify">
+                  <blockquote class="testimonial-2">
+                    <p style="font-size:17px; line-height: 26px;">I take this opportunity to appeal and welcome you for the participation at the “5th National Conference on Innovations in Mechanical Engineering (NCIME- 2017)” during 17th-18th March, 2017 at Sinhgad Institute of Technology, Lonavala. We emphasis on research activities and publish research papers in National/International, Journal/ Conferences. This National Conference has played vital role in connecting with people and researchers of the National Repute since its inception. The main aim of this Conference is to provide platform to network and share innovations in Mechanical Engineering. This conference is also useful to receive constructive feedback on researchers work and to listen eminent speakers from the nation. Thus you are welcome to become an integral part of the initiative to bring the vision of India 2020 to become a reality.<br/>&emsp;&emsp;&emsp; 
+						The papers received and accepted in the earlier conferences are published in conference proceedings. This year we have envisioned publishing accepted papers in the high impact factor, indexed journal of international repute. In addition, we offer reimbursement of one way journey travel expenses from your place to conference venue by the shortest route with standard commuting modes limited to train (sleeper coach) and state transport buses. Furthermore, free on campus accommodation will be provided to you to make your stay hassle free in Lonavala. It may interest you that Lonavala is very popular hill station in this part of the country. Post conference, we are also planning for one day tour of Lonavala-Khandala for all registered participants. <br/>&emsp;&emsp;&emsp;
+						I am sure that this conference will be the most exciting conference you have attended in recent days. Be a part of the change process.</p>
+                  </blockquote>
                 </div>
-                <h3>EMMA GRIFFITH</h3>
-                <span>VP ENGINEERING</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="200ms">
-                <div class="member-image">
-                  <img src="images/team/team-8.jpg" alt="img">
+                
+                <div class="col-md-4 col-md-offset-1">
+                  <div class="ts-author-cont">
+                    <div class="ts-author-info">
+                      <div class="ts-name">
+                        <span class="bold">Dr. V. V. Shinde</span>
+                      </div>
+                      <div class="ts-type">Head of Mechanical Engineering Department, SIT, Lonavala</div>
+                    </div>
+                    <div class="ts-author-img">
+                      <img src="assets/ncime_logo.png" alt="photo">
+                    </div>
+                  </div>
                 </div>
-                <h3>MARY BLACK</h3>
-                <span>HR MANAGER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="400ms">
-                <div class="member-image">
-                  <img src="images/team/team-7.jpg" alt="img">
-                </div>
-                <h3>BRIAN GREEN</h3>
-                <span>ANDROID DEVELOPER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="400ms">
-                <div class="member-image">
-                  <img src="images/team/team-10.jpg" alt="img">
-                </div>
-                <h3>PAUL BISHOP</h3>
-                <span>iOS DEVELOPER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
+          </div>
               
             </div>
 			<div class="row">
 				<div class="heading-underline h3-line">
-						<h3 class="mt-0">Convenor</h3>
-					</div>
-            
-              <div class="member col-md-3 col-sm-3 wow fadeInUp">
-                <div class="member-image">
-                  <img src="images/team/team-6.jpg" alt="img">
+						<h3 class="mt-0">Convener</h3>
+					</div>            
+				<div class="row mb-40">
+                <div class="col-md-7" style="text-align: justify">
+                  <blockquote class="testimonial-2">
+                    <p style="font-size:17px; line-height: 26px;">The 5<sup>th</sup>  National Conference on Innovations in Mechanical Engineering (NCIME- 2017)” is scheduled to be held during 17-18th March 2017 organized by Department of Mechanical Engineering at Sinhgad Institute of Technology, Lonavala. The conference focuses on fundamental research related to design and advances in Mechanical Engineering. The delegates will benefit from exchange of ideas which is the basic agenda of this conference. There has been a good response to the call for papers. Number of research papers has been received from the researchers and academicians of the leading institutes and organizations all over the India. These technical papers give a true account of current research and development trends in the field of Mechanical Engineering.<br/>&emsp;&emsp;&emsp; 
+						The papers received and accepted in the earlier conferences are published in conference proceedings. This year we have envisioned publishing accepted papers in the high impact factor, indexed journal of international repute. In addition, we offer reimbursement of one way journey travel expenses from your place to conference venue by the shortest route with standard commuting modes limited to train (sleeper coach) and state transport buses. Furthermore, free on campus accommodation will be provided to you to make your stay hassle free in Lonavala. It may interest you that Lonavala is very popular hill station in this part of the country. Post conference, we are also planning for one day tour of Lonavala-Khandala for all registered participants. <br/>&emsp;&emsp;&emsp;
+						I am sure that this conference will be the most exciting conference you have attended in recent days. Be a part of the change process.</p>
+                  </blockquote>
                 </div>
-                <h3>EMMA GRIFFITH</h3>
-                <span>VP ENGINEERING</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="200ms">
-                <div class="member-image">
-                  <img src="images/team/team-8.jpg" alt="img">
+                
+                <div class="col-md-4 col-md-offset-1">
+                  <div class="ts-author-cont">
+                    <div class="ts-author-info">
+                      <div class="ts-name">
+                        <span class="bold">Prof. A. G. Kamble</span>
+                      </div>
+                      <div class="ts-type">Associate Professor, Mechanical Engineering</div>
+                    </div>
+                    <div class="ts-author-img">
+                      <img src="assets/ncime_logo.png" alt="photo">
+                    </div>
+                  </div>
                 </div>
-                <h3>MARY BLACK</h3>
-                <span>HR MANAGER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="400ms">
-                <div class="member-image">
-                  <img src="images/team/team-7.jpg" alt="img">
-                </div>
-                <h3>BRIAN GREEN</h3>
-                <span>ANDROID DEVELOPER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-
-              <div class="member col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="400ms">
-                <div class="member-image">
-                  <img src="images/team/team-10.jpg" alt="img">
-                </div>
-                <h3>PAUL BISHOP</h3>
-                <span>iOS DEVELOPER</span>
-                <ul class="team-social">
-                  <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
-                  <li><a href="#"><span aria-hidden="true" class="social_dribbble"></span></a></li>
-                </ul>
-              </div>
-              
+          </div>
             </div>
 			<div class="row">
 				<div class="heading-underline h3-line">
