8b78538a2d5d87650ae88e779bad557dad8ec310
Sudha <suda_amir@hotmail.com>
2016-12-05 10:12:00 +0530

    Keynote Speakers

---
+++ keynote_speakers.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+ 	
+
+    <?php require_once ("include_header.php"); ?>
+    <body>
+	<?php require_once ("include_preloader.php"); ?>
+
+		<div id="wrap" class="boxed ">
+			<div class="grey-bg"> <!-- Grey BG  -->	
+				
+<?php require_once ("include_ie_warning.php"); ?>				
+				<!-- HEADER 1 NO-TRANSPARENT -->
+				<?php require_once ("include_navbar.php"); ?>  
+        <!-- PAGE TITLE BIG IMG -->
+        <div class="page-title-cont page-title-big page-title-img grey-dark-bg" style="background-image: url(assets/schedule.jpg)">
+          <div class="relative container align-left">
+            <div class="row">
+               
+              <div class="col-md-8">
+                <h1 class="page-title">Review Committee</h1>
+              </div>
+               
+              <div class="col-md-4">
+                <div class="breadcrumbs">
+                  <a href="index.html">Home</a><span class="slash-divider">/</span><span class="bread-current">Review Committee</span>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+ 
+        <!-- CONTAINER -->
+        <div class="container p-140-cont">
+
+          <div class="row">
+			  				<h4 style="text-align:center">CONTENT YET TO BE GIVEN</h4>
+
+            
+          </div>		
+        </div>
+        
+        
+        <?php require_once ("include_footer.php"); ?>
+        
+				<!-- BACK TO TOP -->
+				<p id="back-top">
+          <a href="#top" title="Back to Top"><span class="icon icon-arrows-up"></span></a>
+        </p>
+        
+			</div><!-- End BG -->	
+		</div><!-- End wrap -->	
+			
+<?php require_once ("include_js.php"); ?>
+	
+	</body>
+
+<!-- Mirrored from abcgomel.ru/haswell-1.8.1-demo/page-title-big-img.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 01 Dec 2016 17:16:38 GMT -->
+</html>		
